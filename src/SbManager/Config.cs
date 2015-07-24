1383d76fdc76daa8729fa67094e8ecceac8a0269
Robert Wagner <robert.wagner@daf.qld.gov.au>
2015-07-24 15:39:03 +1000

    Added by-convention local service bus

--- src/SbManager/Config.cs
+++ src/SbManager/Config.cs
@@ -1,5 +1,7 @@
 ﻿using System.Configuration;
 using System.IO;
+using System.Net;
+using System.Net.NetworkInformation;
 using System.Text.RegularExpressions;
 
 namespace SbManager
@@ -20,7 +22,10 @@ namespace SbManager
 
                 var connectionString = ConfigurationManager.ConnectionStrings["busConnectionString"].ConnectionString;
 
-                if (new Regex("^[a-z]:|^//", RegexOptions.IgnoreCase).IsMatch(connectionString))
+
+                if (connectionString == "local")
+                    connectionString = BuildLocalEndpoint();
+                else if (new Regex("^[a-z]:|^//", RegexOptions.IgnoreCase).IsMatch(connectionString))
                     connectionString = File.ReadAllText(connectionString);
 
                 _connectionString = connectionString;
@@ -30,6 +35,27 @@ namespace SbManager
         public string WebAppUrl
         {
             get { return ConfigurationManager.AppSettings["WebAppUrl"]; }
+        }
+
+        private static string BuildLocalEndpoint()
+        {
+            var hostname = GetHostnameAndDomainName();
+            return $"Endpoint=sb://{hostname}/FamsBus;StsEndpoint=https://{hostname}:9355/FamsBus;RuntimePort=9354;ManagementPort=9355;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=xadfN52mfTvng5HJak8Dnv9dfg9wizCXPqtpYi05Iwo=";
+        }
+
+
+        public static string GetHostnameAndDomainName()
+        {
+            // if No domain name return a generic string           
+            string currentDomainName = IPGlobalProperties.GetIPGlobalProperties().DomainName ?? "nodomainname";
+            string hostName = Dns.GetHostName();
+
+            // check if current hostname does not contain domain name
+            if (!hostName.Contains(currentDomainName))
+            {
+                hostName = hostName + "." + currentDomainName;
+            }
+            return hostName.ToLower();  // Return combined hostname and domain in lowercase
         }
     }
 }
